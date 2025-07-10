4c20522b233850fc2521a59cabf91389b5df027c
gabriel-silveira <gabriel@gabrielsilveira.com.br>
2025-07-10 12:50:15 -0300

    Using ToolManager instead of ArcadeToolManager (deprecated)

--- src/tools.py
+++ src/tools.py
@@ -1,8 +1,8 @@
-from langchain_arcade import ArcadeToolManager
+from langchain_arcade import ToolManager
 from langgraph.prebuilt import ToolNode
 from src.config import ARCADE_API_KEY
 
 # Initialize the tool manager and fetch tools compatible with langgraph
-tool_manager = ArcadeToolManager(api_key=ARCADE_API_KEY)
-tools = tool_manager.get_tools(toolkits=["Google"])
+tool_manager = ToolManager(api_key=ARCADE_API_KEY)
+tools = tool_manager.init_tools(toolkits=["Google"])
 tool_node = ToolNode(tools)
